d20a0534baaa5a69041c36382607ee558297c3cd
israelViner <114855847+israelViner@users.noreply.github.com>
2022-10-02 16:08:42 +0300

    Update Node.java

--- bank/Node.java
+++ bank/Node.java
@@ -1,12 +1,12 @@
 
 public class Node
 {
-    //propertires
+//propertires
     private key x;
     private int height;
     private Node _leftSon, _rightSon;
     
-    // constructor
+// constructor
     public Node(int ID, String name) {
         this.x = new key(ID, name);
         this.height = 1;
@@ -14,14 +14,14 @@ public class Node
         this._rightSon = null;
     }
     
-    // the get methods
+// the get methods
     public int getHeight(Node t) {
         if (t == null)
            return 0;
         return t.height;
     }
     
-        public Node getLeftSon() {
+    public Node getLeftSon() {
         return this._leftSon;
     }
     
@@ -37,7 +37,7 @@ public class Node
         return this.x.getBalance();
     }
     
-    // the set methods
+// the set methods
     public void setHeight() {
         this.height = (this.getHeight(this.getRightSon()) > this.getHeight(this.getLeftSon()))? (1 + this.getHeight(this.getRightSon())): 1 + this.getHeight(this.getLeftSon());
         //this.height = 1 + Math.max(this.getHeight(this.getRightSon()), this.getHeight(this.getLeftSon()));
@@ -55,7 +55,7 @@ public class Node
         this.x.setBalance(sum);
     }
     
-} //end of class Node
+} 
     
 
     
